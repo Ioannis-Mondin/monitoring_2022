0025a046f091b4e2687c6073be735e6687f6e2f5
Ioannis <92546815+Ioannis-Mondin@users.noreply.github.com>
2021-10-29 11:02:03 +0200

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -7,3 +7,20 @@ install.packages("raster")
 # no quotes because its inside R (we previously installed it
 library(raster)
 
+setwd("C:/lab/")
+
+# we are going to import satellite data
+l2011 <- brick("p224r63_2011.grd")
+#objects can not be numbers!!
+plot(l2011)
+
+# B1 is the reflectance in the blue band
+# B2 is the reflectance in the green band
+# B3 is the reflectance in the red band
+
+cl <- colorRampPalette(c("black", "grey", "light grey"))(100)
+plot(l2011, col=cl)
+
+plotRGB(l2011, r=3, g=2, b=1, stretch="Lin")
+
+# RGB all colors that human sees, kind of natural...
