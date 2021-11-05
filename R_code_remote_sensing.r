617e9217ba6bc6f625434ea4e42cbc453597df91
Ioannis <92546815+Ioannis-Mondin@users.noreply.github.com>
2021-11-05 11:03:28 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -7,6 +7,7 @@ install.packages("raster")
 # no quotes because its inside R (we previously installed it
 library(raster)
 
+# set the working diretory
 setwd("C:/lab/")
 
 # we are going to import satellite data
@@ -17,6 +18,7 @@ plot(l2011)
 # B1 is the reflectance in the blue band
 # B2 is the reflectance in the green band
 # B3 is the reflectance in the red band
+# B4 is the reflectance in the deep red (NIR) band
 
 cl <- colorRampPalette(c("black", "grey", "light grey"))(100)
 plot(l2011, col=cl)
@@ -24,3 +26,32 @@ plot(l2011, col=cl)
 plotRGB(l2011, r=3, g=2, b=1, stretch="Lin")
 
 # RGB all colors that human sees, kind of natural...
+
+l2011 <- brick("p224r63_2011.grd")
+l2011
+
+# sre means spectural reflectance
+# we link with the $ !!!
+# let's plot the green band
+plot(l2011 $ B2_sre) 
+
+cl <- colorRampPalette(c("black", "grey", "light grey"))(100)
+plot(l2011$B2_sre, col=cl)
+
+# change the colorRampPalette with dark green and light green
+clg <- colorRampPalette(c("dark green", "green", "light green"))(100)
+plot(l2011$B2_sre, col=clg)
+
+# change the colorRampPalette with dark blue, blue and light blue (change also the name -B2_sre-)
+clb <- colorRampPalette(c("dark blue", "blue", "light blue"))(100)
+
+# Ultra useful stuff below!
+# plot both images in just one multiframe graph
+par(mfrow(1,2))
+plot(l2011 $ B1_sre, col=clb) 
+plot(l2011 $ B2_sre, col=clg) 
+
+# plot both images in just one multiframe graph with two rows and just one column
+par(mfrow=c(2,1))
+plot(l2011$B1_sre, col=clb)
+plot(l2011$B2_sre, col=clg)
