886c1f380d86bf394c0fba96f7f6c1326ae9677b
Ioannis <92546815+Ioannis-Mondin@users.noreply.github.com>
2021-11-26 10:08:57 +0100

    Create R_code_chemical_cycling.r

---
+++ R_code_chemical_cycling.r
@@ -0,0 +1,13 @@
+# R code for chemical cycling study
+# time series of NO2 change in Europe during the lockdown
+
+setwd("C:/lab/EN/")
+
+library(raster)
+
+en01 <- raster("EN_0001.png")
+en01
+
+# what is the range of the 
+# 8bit images: values (0,255) (2^8 -1)
+cl <- colorRampPalette(c('deepskyblue3','aquamarine3','yellow'))(100)
