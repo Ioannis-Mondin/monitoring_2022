2ae3d9889f53f0cb8a8cf6e5d0472911538b419e
Ioannis <92546815+Ioannis-Mondin@users.noreply.github.com>
2021-11-26 10:15:28 +0100

    Update R_code_chemical_cycling.r

--- R_code_chemical_cycling.r
+++ R_code_chemical_cycling.r
@@ -11,3 +11,10 @@ en01
 # what is the range of the 
 # 8bit images: values (0,255) (2^8 -1)
 cl <- colorRampPalette(c('deepskyblue3','aquamarine3','yellow'))(100)
+
+> plot(en01, col=cl)
+
+en13 <- raster("EN_0013.png")
+> plot(en13, col=cl)
+
+
