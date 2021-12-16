b6e88fbd3593fb904edf354cd929224f1f19cfc7
Ioannis <92546815+Ioannis-Mondin@users.noreply.github.com>
2021-12-16 12:43:30 +0100

    Update R_code_copernicus.r

--- R_code_copernicus.r
+++ R_code_copernicus.r
@@ -4,3 +4,17 @@ setwd("C:/lab/copernicus/")  # windows
 lai20200625 <- raster("c_gls_LAI_202006130000_GLOBE_PROBAV_V1.5.1.nc")
 lai20200625
 plot(lai20200625)
+
+cl <- colorRampPalette(c("dark blue","blue","light blue"))(100)
+plot(lai20200625, col=cl)
+library(viridis)
+
+library(ggplot2)
+library(RStoolbox)
+
+ggplot() +
+geom_raster(lai20200625, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent))
+
+ggplot() +
+geom_raster(snow20211214, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent)) +
+scale_fill_viridis()
